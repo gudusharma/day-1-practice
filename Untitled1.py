a8f56267219eb1c7e4515c598f5d9d5dde6ae088
Dev Sharma <115183867+gudusharma@users.noreply.github.com>
2022-10-08 23:42:56 +0530

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#user input-------->
+x=input("Enter something!!")
+print(x)
+
+
+# In[2]:
+
+
+#wap to add two numbers using user input
+a=int(input("Enter the first num"))
+b=int(input("Enter the second num"))
+c=a+b
+print(c)
+
+
+# In[20]:
+
+
+#condition
+#if condition
+#print the message
+#else
+#print the msg
+
+age=2
+if age>18:
+    print ("today is vote!!")
+else:
+    print("no vote")
+
+
+# In[13]:
+
+
+print("Ram's book")
+
+
+# In[22]:
+
+
+#wap to find a individual is achild or adult
+a=int(input("Enter the age"))
+if a>18:
+    print("Adult")
+else:
+    print("child")
+        
+
+
+# In[30]:
+
+
+exp=int(input("Enter your exp"))
+sal=int(input("Enter your salary"))
+
+if exp>5:
+    bonus=sal*5/100
+    sal=sal+bonus
+    print(total salary)
+    print("Bonus amount=",bonus)
+else:
+    print("no bonus")
+
+
+# In[ ]:
+
+
+
+
+
+# In[31]:
+
+
+#wap to print find the if it is rainy outside or not
+#condition is if temp is 20 degree then it is rainy
+
+temp=45
+if temp==45:
+    print("cool weather")
+
+
+# In[1]:
+
+
+exp = int(input("Enter your exp"))
+sal = int(input("Enter your salary"))
+
+exp = 6
+sal = 3000
+
+if exp>5:
+    bonus = sal * 5/ 100 
+    print("Bonus amount=",bonus)
+    
+else:
+    print("No bonus")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
